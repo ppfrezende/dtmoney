8614d7843aaef509af9362dc6238eb3c24ad2923
Fellipe Rezende <rezendefellipe9@gmail.com>
2021-04-13 17:13:08 -0300

    :heavy_check_mark: Created header styles

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import LogoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
+
+export function Header() {
+  return (
+    <Container>
+      <Content>
+        <img src={LogoImg} alt="dt Money" />
+        <button type="button">Nova Transação</button>
+      </Content>
+    </Container>
+  );
+}
