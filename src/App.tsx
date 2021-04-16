3faf3f07564f896cfe1a75fa0a0fe0a8d47e81c1
Fellipe Rezende <rezendefellipe9@gmail.com>
2021-04-16 10:50:08 -0300

    :sparkles: ContextApi initial configs

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Dashboard } from './components/Dashboard';
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './styles/global';
+import { TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -23,7 +24,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -33,6 +34,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
